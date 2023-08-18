2ee1103ae9be003e039a1af42d62f45c7b0e7fc0
鎗田祐生 <yariyuu45@gmail.com>
2023-08-18 09:18:14 +0900

    第３章課題提出
    チェックテスト

---
+++ eclipse/eclipse-workspace/frksCheck/3-1/chapter3/CheckTest3.java
@@ -0,0 +1,38 @@
+package chapter3;
+
+public class CheckTest3 {
+  public static void main(String[] args){
+     /*
+        * 問1
+        * int型の配列dataを作成し、値を3,1,2,7,5で初期化しなさい
+        */
+        int[] data = {3, 1, 2, 7, 5};
+
+        /*
+        * 問2
+        * 以下のfor文を完成させなさい
+        */
+        for(int i = 0; i < data.length; i++){ //int型のiを0で初期化。iがdata配列の要素数より小さい時iに1ずつ足していく
+          System.out.print(data[i] + " ");  //data配列の要素を一つずつ取り出して表示する
+        }
+      System.out.println();
+      for (int i = 0; i < data.length - 1; i++) { //int型のiを0で初期化。iがdata配列から1を引いた要素数より小さい時iに1ずつ足していく（配列が０番目からスタートするため）
+          for (int j = data.length - 1; j > i; j--) { //int型のjにdata配列から1を引いた要素数で初期化。（配列が０番目からスタートするため）jがiより小さい時jから1ずつ引いていく
+              /*
+              * 問3
+              * 以下、配列の添字を入れてソートを完成させなさい
+              */
+              if(data[j - 1] > data[j]){ //data配列jから1を引いた値(要素番号)がdata配列jより大きい時の条件
+                int box = data[j];  //int型のboxをdata配列のjの値で初期化。
+
+                data[j] = data[j - 1];  //data[j]にdata[j - 1]を代入
+
+                data[j - 1] = box;  //data[j - 1]にboxを代入
+              }
+          }
+      }
+      for(int i = 0; i < data.length; i++){  //int型のiを0で初期化。iがdata配列の要素数より小さい時iに1ずつ足していく（配列番号0（1）から4（5）まで）
+          System.out.print(data[i] + " ");  //data[i]で配列の上記で指定した配列要素を持ってきて表示させる
+      }
+  }
+}
---
+++ eclipse/eclipse-workspace/frksCheck/4-1/check/Main.java
@@ -0,0 +1,45 @@
+package check;
+
+import constants.Constants;
+
+
+public class Main {
+
+//	課題①
+//	【Main.java】にてフィールド変数firstNameとlastNameを宣言し、
+//	firstName →　自分の名字　lastName →　自分の名前で初期化しなさい。
+//	なお、変数のアクセス修飾子は「private」とする。
+	private String firstName = "Yarita";
+	private String lastName = "Yuki";
+
+	public static void main(String[] args) {
+
+		Main main = new Main();
+		main.printName(main.firstName, main.lastName);
+		Pet pet = new Pet(Constants.CHECK_CLASS_JAVA,Constants.CHECK_CLASS_HOGE);
+		RobotPet rp = new RobotPet(Constants.CHECK_CLASS_R2D2,Constants.CHECK_CLASS_LUKE);
+		pet.introduce();
+		rp.introduce();
+		
+	}
+
+//	課題②
+//	【Main.java】にてfirstNameとlastNameを引数で受け取って、
+//	連結して表示させるメソッド「printName」を作成しなさい。
+//	作成した関数のアクセス修飾子は「private」とする。
+	private void printName(String firstName, String lastName) {
+		
+		System.out.println(firstName + lastName);
+
+//		課題③
+//		【Main.java】にてPetクラスとRobotPetクラスをインスタンス化して、下記の完成イメージを出力させなさい。	
+	/*
+	 * printNameメソッド→七海真弥 
+	 * ■僕の名前はjava吉です 
+	 * ■ご主人様はhogeです 
+	 * ◇私はロボット。名前はR2D2。 
+	 * ご主人様はルーク。
+	 */
+		
+}
+}
